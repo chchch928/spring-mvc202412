76a681dec162d2e49175b6d654c7a5aaa56647b6
chchch928 <kbanda@naver.com>
2024-12-16 11:22:32 +0900

    12-16 초기세팅

---
+++ src/main/java/com/spring/mvcproject/MvcProjectApplication.java
@@ -0,0 +1,14 @@
+package com.spring.mvcproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcProjectApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MvcProjectApplication.class, args);
+    }
+
+}
